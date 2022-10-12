e174fe17ff32fe0a79513cde08d373dfb137f325
wolla22 <95152638+wolla22@users.noreply.github.com>
2022-10-12 19:48:41 -0400

    Add files via upload

---
+++ flutter_working_examples/flutter_working_examples/movie/lib/http_helper.dart
@@ -0,0 +1,41 @@
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'movie.dart';
+
+class HttpHelper {
+  final String urlKey = 'api_key=8361ad82497ec1cf55ca10b74f1d3750';
+  final String urlBase = 'https://api.themoviedb.org/3/movie';
+  final String urlUpcoming = '/upcoming?';
+  final String urlLanguage = '&language=en-US';
+  final String urlSearchBase =
+      'https://api.themoviedb.org/3/search/movie?api_key=8361ad82497ec1cf55ca10b74f1d3750&query=';
+
+  Future<List> getUpcoming() async {
+    final String upcoming = urlBase + urlUpcoming + urlKey + urlLanguage;
+    http.Response result = await http.get(upcoming);
+    var myResult = await http.get(upcoming);
+
+    if (result.statusCode == HttpStatus.ok) {
+      final jsonResponse = json.decode(result.body);
+      final moviesMap = jsonResponse['results'];
+      List movies = moviesMap.map((i) => Movie.fromJson(i)).toList();
+      return movies;
+    } else {
+      return null;
+    }
+  }
+
+  Future<List> findMovies(String title) async {
+    final String query = urlSearchBase + title;
+    http.Response result = await http.get(query);
+    if (result.statusCode == HttpStatus.ok) {
+      final jsonResponse = json.decode(result.body);
+      final moviesMap = jsonResponse['results'];
+      List movies = moviesMap.map((i) => Movie.fromJson(i)).toList();
+      return movies;
+    } else {
+      return null;
+    }
+  }
+}
